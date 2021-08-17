1855f2ec4897017d2b838fbe953e7e55e9f38968
PaulaSena <senasilvaanapaula@gmail.com>
2021-08-17 14:15:38 -0300

    Clase interna
    En la clase interna es posible crear una integracíon de una API EX:Endereço  usando Json e Objeto Response.

--- 2 Semana/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ 2 Semana/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,13 +3,18 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Ana Paula Sena"
     var cpf: String = "012.345.678.90"
-}
 
+inner class Endereco{   // Clase interna
+        var rua: String = "Rua tal"
+    }
+}
 fun main() {
     val paulaSena = Pessoa()  //Variable que recibe la instancia de una persona
 
     println(paulaSena.nome)
     println(paulaSena.cpf)
+    println(paulaSena.Endereco().rua)  // Instanciar una Clase interna
 }
 
 // Commit Ctrl + K ...
+// En la clase interna es posible realizar una integración de uma API usando o Json creando un Objeto de Response (Dividido) parcial.

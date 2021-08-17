40601dcdb80c0abf671f2936548cb332ae6df491
PaulaSena <senasilvaanapaula@gmail.com>
2021-08-17 19:00:36 -0300

    constructor() // Miembros de la clase

--- 2 Semana/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ 2 Semana/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,19 +3,32 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Ana Paula Sena"
     var cpf: String = "012.345.678.90"
-    private set // Esta función evita cambiar el valor cpf
-
+        private set // Esta función evita cambiar el valor cpf
+    /* *
 inner class Endereco{   // Clase interna
         var rua: String = "Rua tal"
     }
+*/
+    constructor() // Miembros de la clase
+
+    fun pessoaInfo () = "$nome e $cpf"
+
 }
+
+
 fun main() {
     val paulaSena = Pessoa()  //Variable que recibe la instancia de una persona
 
-    println(paulaSena.nome)
-    println(paulaSena.cpf)
-    println(paulaSena.Endereco().rua)  // Instanciar una Clase interna
+    println(paulaSena.pessoaInfo())
+
+    // println(paulaSena.nome)
+    //println(paulaSena.cpf)
+ /* *  println(paulaSena.Endereco().rua)  // Instanciar una Clase interna*/
 }
+
+
+
+
 
 // Commit Ctrl + K ...
 

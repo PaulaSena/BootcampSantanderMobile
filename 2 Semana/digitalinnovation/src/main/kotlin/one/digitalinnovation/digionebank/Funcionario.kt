fa5354c7e8110a5c0bf2596616bd15663ae48147
PaulaSena <senasilvaanapaula@gmail.com>
2021-08-20 11:55:40 -0300

    Transformando la class Pessoa.kt e Funcionario.kt en Abstrata |  ** En la classe abstrata no es posible instanciar **  Eliminada las classes de Prueba.

--- 2 Semana/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ 2 Semana/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -7,7 +7,7 @@ abstract class Funcionario (
     override val cpf: String,
     val salario:BigDecimal
 ) : Pessoa(nome, cpf) {
-    abstract fun calculoAuxilio();  // LA CLASE DE PRUEBA SE QUEDARÁ AQUÍ
+    abstract fun calculoAuxilio(); // LA CLASE DE PRUEBA SE QUEDARÁ AQUÍ
 }
 
 // Transformando la clase Open Funcionario en Abstrata  para ser la "Clase hija de la Persona"

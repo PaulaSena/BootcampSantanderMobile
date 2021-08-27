4fa8123f1bd476f14212ef4efa4be8eaa9fd79da
PaulaSena <senasilvaanapaula@gmail.com>
2021-08-27 11:36:46 -0300

    Primeiro Projeto Android Kotlin

---
+++ 2 Semana/conta/ContaPessoaFisica.kt
@@ -0,0 +1,21 @@
+    package conta
+
+
+    //Mentoria1
+
+    class ContaPessoaFisica(
+        numero:Int,
+        agencia:String
+    ) {
+        init {
+            println("nun: $numero - agencia: $agencia")
+        }
+    }
+    fun main() {
+        val conta1 = ContaPessoaFisica(numero = 123, agencia = "001")
+
+        var conta2 = ContaPessoaFisica(numero = 124,agencia = "002")
+
+        var conta3 = ContaPessoaFisica(numero = 125,agencia = "001")
+
+    }

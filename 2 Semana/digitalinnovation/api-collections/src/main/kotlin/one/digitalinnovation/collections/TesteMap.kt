af60fd632f3d65bb1e4a737c9cf3eb8ef623624b
PaulaSena <senasilvaanapaula@gmail.com>
2021-09-09 18:40:43 -0300

    TesteMap.kt | Desenvolvimento com mapOf Utilizando o Pair MapOf e forEach

---
+++ 2 Semana/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val pair: Pair<String, Double> = Pair ("Théo", 1000.0)
+    val map1= mapOf(pair)
+
+    map1.forEach { (k, v) ->  println("Chave: $k - Valor: $v")}
+    val map2 = mapOf(
+        "Pedro" to 2500.0,
+        "Sofia" to 3000.0
+    )
+    map2.forEach { (k, v ) -> println ("Chave: $k - Valor: $v")}
+}

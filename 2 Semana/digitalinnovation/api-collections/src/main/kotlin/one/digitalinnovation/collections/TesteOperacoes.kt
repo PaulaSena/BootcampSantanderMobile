d58a1795fb8333f16e3698f3c481675dcb228c7e
PaulaSena <senasilvaanapaula@gmail.com>
2021-09-09 01:28:37 -0300

    TesteOperacoes.kt |  Operações com Arrays max, min, average média e filter filtra os dados conforme expressão | Operações com Arrays count range realiza a contagem, find encontra o valor exato e any encontra o valor true ou false

--- 2 Semana/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ 2 Semana/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -16,4 +16,25 @@ fun main() {
     println("Salarios acima de 2500.0")
 
     salariosMaiorQue2500.forEach { println(it) }
+
+    // Operações com Arrays count, find e any
+
+    println("==============")
+    println("Quantos salarios estao entre 2000.0 e 5000.0 dentro do array")
+
+    // Quantos salarios estao entre 2000.0 e 5000.0 dentro do array Range
+    println(salarios.count{it in 2000.0..5000.0 }) //range realiza a contagem
+
+    println("==============")
+    println("Encontre esse valor")
+
+    println(salarios.find{it ==3700.0 }) //find encontra o valor exato
+    println(salarios.find{it ==700.0 }) //find encontra o valor exato
+
+    println("==============")
+    println("Encontre qualqr valor verdadeiro")
+
+    println(salarios.any{it ==3700.0 }) //any encontra o valor true ou false
+    println(salarios.any{it ==700.0 }) //any encontra o  valor true ou false
+
 }

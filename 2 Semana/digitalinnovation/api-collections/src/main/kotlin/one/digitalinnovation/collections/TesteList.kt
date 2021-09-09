0158e8b211e6e441b51e81bc4cf31753d8a1bf5a
PaulaSena <senasilvaanapaula@gmail.com>
2021-09-09 02:05:19 -0300

    TesteList.kt | Organizando grupos com sortedBy  funcionarios.sortedBy { it.salario } // Ordenando o objeto por salario do menor para o maior                        .forEach { println(it) } // finaliza todas as operações

--- 2 Semana/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ 2 Semana/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val ana = Funcionario(nome = "Ana Paula", salario = 3000.0 )
-    val bia = Funcionario(nome = "Bianca", salario = 3100.0 )
-    val pedro = Funcionario(nome =  "Pedro" , salario = 3200.0 )
+    val ana = Funcionario(nome = "Ana Paula", salario = 6000.0 )
+    val bia = Funcionario(nome = "Bianca", salario = 9100.0 )
+    val pedro = Funcionario(nome =  "Pedro" , salario = 5200.0 )
 
     val funcionarios = listOf(ana, bia, pedro)
 
@@ -11,6 +11,12 @@ fun main() {
     // Buscando só a Ana
     println("==============")
     println(funcionarios.find { it.nome =="Ana Paula" })
+
+
+    println("===== sortedBy =========")
+    funcionarios
+        .sortedBy { it.salario } // Ordenando o objeto por salario do menor para o maior
+        .forEach { println(it) } // finaliza todas as operações
 
 }
 

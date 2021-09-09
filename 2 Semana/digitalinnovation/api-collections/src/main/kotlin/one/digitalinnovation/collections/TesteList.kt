f912bcb5c93cf5d6b4d924ae172b8ea68c5c544b
PaulaSena <senasilvaanapaula@gmail.com>
2021-09-09 12:04:14 -0300

    TesteList.kt | groupBy Agrupando o objeto por tipo de contratação

--- 2 Semana/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ 2 Semana/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val ana = Funcionario(nome = "Ana Paula", salario = 6000.0 )
-    val bia = Funcionario(nome = "Bianca", salario = 9100.0 )
-    val pedro = Funcionario(nome =  "Pedro" , salario = 5200.0 )
+    val ana = Funcionario(nome = "Ana Paula", salario = 6000.0, tipoContratacao = "PJ" )
+    val bia = Funcionario(nome = "Bianca", salario = 9100.0,tipoContratacao = "CLT" )
+    val pedro = Funcionario(nome =  "Pedro" , salario = 5200.0,tipoContratacao = "CLT" )
 
     val funcionarios = listOf(ana, bia, pedro)
 
@@ -12,17 +12,19 @@ fun main() {
     println("==============")
     println(funcionarios.find { it.nome =="Ana Paula" })
 
-
     println("===== sortedBy =========")
     funcionarios
         .sortedBy { it.salario } // Ordenando o objeto por salario do menor para o maior
         .forEach { println(it) } // finaliza todas as operações
-
+    println("===== groupBy =========")
+    funcionarios
+        .groupBy { it.tipoContratacao } // Agrupando o objeto por tipo de contratação
+        .forEach { println(it) } // finaliza todas as operações
 }
-
 data class Funcionario(
     val nome:String,
-    val salario:Double
+    val salario:Double,
+    val tipoContratacao:String
 ){
     override fun toString(): String ="""
         Nome: $nome
